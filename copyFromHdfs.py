cb9cfdda703f5f1c56f2dd76d235b55c65e91371
Kenneth Long <kdlong@hep.wisc.edu>
2017-05-29 14:05:34 +0200

    Ntuple update across many files

--- copyFromHdfs.py
+++ copyFromHdfs.py
@@ -12,13 +12,17 @@ parser.add_argument("-s", "--selection", type=str, default="FinalSelection",
 args = parser.parse_args()
 
 for directory in glob.glob(args.path):
-    print directory
+    print "Copying directory", directory
     if not os.path.isdir(directory):
         print "Invalid filename: %s" % directory
         exit(1)
     dir_name = directory.split("/")[-1]
     new_dir = "/data/kelong/DibosonAnalysisData/%s/%s" % (args.selection, dir_name)
-    os.mkdir(new_dir)
+    try:
+        os.mkdir(new_dir)
+    except OSError as e:
+        print e
+        continue
     for filename in glob.glob(directory + "/*"):
         subprocess.call(["xrdcp", 
             "root://cmsxrootd.hep.wisc.edu/%s" % filename[5:], 
