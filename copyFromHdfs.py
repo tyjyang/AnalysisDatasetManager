0428086cbdfcf20c8b1fc0deec19de0ce8d699fe
Kenneth Long <kdlong@hep.wisc.edu>
2017-08-17 11:50:52 +0200

    Add 3MediumLeptons selection

--- copyFromHdfs.py
+++ copyFromHdfs.py
@@ -4,9 +4,11 @@ import argparse
 import glob
 import os
 import subprocess
+import multiprocessing
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", type=str, help="path to files in hdfs")
+parser.add_argument("--eos", action='store_true', help="Store files on eos instead of /data")
 parser.add_argument("-s", "--selection", type=str, default="FinalSelection",
         help="Selection tier (default FinalSelection)")
 args = parser.parse_args()
@@ -16,15 +18,24 @@ for directory in glob.glob(args.path):
     if not os.path.isdir(directory):
         print "Invalid filename: %s" % directory
         exit(1)
-    dir_name = directory.split("/")[-1]
+    dirs = directory.split("/")
+    dir_name = dirs[-1]
+    if dir_name[:3] == "000" and len(dirs)>= 5:
+        if "/hdfs" in directory:
+            dir_name = "_".join(dirs[6:8])
+        else:
+            dir_name = "_".join(dirs[5:7])
     new_dir = "/data/kelong/DibosonAnalysisData/%s/%s" % (args.selection, dir_name)
+    if args.eos:
+        new_dir = "/eos/user/k/kelong/%s/%s" % (args.selection, dir_name)
     try:
         os.mkdir(new_dir)
     except OSError as e:
         print e
         continue
-    for filename in glob.glob(directory + "/*"):
-        subprocess.call(["xrdcp", 
-            "root://cmsxrootd.hep.wisc.edu/%s" % filename[5:], 
-            new_dir]
+    filenames = [f if "hdfs" not in f else f[5:] for f in glob.glob(directory + "/*.root")]
+    p = multiprocessing.Pool(processes=10)
+    p.map(subprocess.call, [["xrdcp", 
+            "root://cmsxrootd.hep.wisc.edu/%s" % filename, 
+            new_dir] for filename in filenames]
         )
