99048df0f97fe4faf0504838f0e61b9412f86f6e
Kenneth Long <kdlong@hep.wisc.edu>
2019-07-02 00:01:21 -0500

    Add ZGen jet variables

--- PlotGroups/ZGen.py
+++ PlotGroups/ZGen.py
@@ -1,9 +1,16 @@
 info = {
+    "dy_nlo" : {
+        "Name" : "Drell-Yan (MG NLO)",
+        "Style" : "nofill-black-dotdash",
+        "Members" : [
+            "DYm50"
+        ]
+    },
     "dy_lo" : {
         "Name" : "Drell-Yan (MG LO)",
         "Style" : "nofill-yellow",
         "Members" : [
-            "DYm50"
+            "DYm50__LO"
         ]
     },
 }
--- PlotObjects/ZGen.py
+++ PlotObjects/ZGen.py
@@ -2,7 +2,7 @@ info = {
     "yZ": {  
         "Initialize": {  
             "type": "TH1F",
-            "nbins": 24,
+            "nbins": 56,
             "xmin": -3,
             "xmax": 3
         },
@@ -32,12 +32,156 @@ info = {
    "ptl1": {  
         "Initialize": {  
             "type": "TH1D",
+            "nbins": 160,
+            "xmin": 0,
+            "xmax": 160
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "Gen Lepton p_{T} [GeV]",  
+            "GetYaxis().SetTitle": "Events / GeV",
+            "GetYaxis().SetTitleOffset": 1.2  
+        }
+    },
+   "ptl2": {  
+        "Initialize": {  
+            "type": "TH1D",
             "nbins": 120,
             "xmin": 0,
             "xmax": 120
         },
         "Attributes": {  
             "GetXaxis().SetTitle": "Gen Lepton p_{T} [GeV]",  
+            "GetYaxis().SetTitle": "Events / GeV",
+            "GetYaxis().SetTitleOffset": 1.2  
+        }
+    },
+    "etal1": {  
+        "Initialize": {  
+            "type": "TH1F",
+            "nbins": 20,
+            "xmin": -2.5,
+            "xmax": 2.5
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "\\eta^{\\ell_1}",  
+            "GetYaxis().SetTitle": "Events", 
+            "GetYaxis().SetTitleOffset": 1.3,
+            "SetMinimum" : 0.1,
+            "SetMaximum" : 7000000
+        }
+    },
+    "etal2": {  
+        "Initialize": {  
+            "type": "TH1F",
+            "nbins": 20,
+            "xmin": -2.5,
+            "xmax": 2.5
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "\\eta^{\\ell_2}",  
+            "GetYaxis().SetTitle": "Events", 
+            "GetYaxis().SetTitleOffset": 1.3,
+            "SetMinimum" : 0.1,
+            "SetMaximum" : 7000000
+        }
+    },
+   "ptj1": {  
+        "Initialize": {  
+            "type": "TH1D",
+            "nbins": 40,
+            "xmin": 20,
+            "xmax": 420
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "Gen jet 1 p_{T} [GeV]",  
+            "GetYaxis().SetTitle": "Events / GeV",
+            "GetYaxis().SetTitleOffset": 1.2,
+            "SetMinimum" : 0.1,
+            "SetMaximum" : 7000000
+        }
+    },
+   "ptj2": {  
+        "Initialize": {  
+            "type": "TH1D",
+            "nbins": 40,
+            "xmin": 20,
+            "xmax": 420
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "Gen jet 2 p_{T} [GeV]",  
+            "GetYaxis().SetTitle": "Events / GeV",
+            "GetYaxis().SetTitleOffset": 1.2,
+            "SetMinimum" : 0.1,
+            "SetMaximum" : 7000000
+        }
+    },
+    "etaj1": {  
+        "Initialize": {  
+            "type": "TH1F",
+            "nbins": 40,
+            "xmin": -5,
+            "xmax": 5
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "\\eta^{\\mathrm{j}_1}",  
+            "GetYaxis().SetTitle": "Events", 
+            "GetYaxis().SetTitleOffset": 1.3,
+            "SetMinimum" : 0.1,
+            "SetMaximum" : 4000000
+        }
+    },
+    "etaj2": {  
+        "Initialize": {  
+            "type": "TH1F",
+            "nbins": 40,
+            "xmin": -5,
+            "xmax": 5
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "\\eta^{\\mathrm{j}_2}",  
+            "GetYaxis().SetTitle": "Events", 
+            "GetYaxis().SetTitleOffset": 1.3,
+            "SetMinimum" : 0.1,
+            "SetMaximum" : 2000000
+        }
+    },
+    "nJets": {  
+        "Initialize": {  
+            "type": "TH1F",
+            "nbins": 10,
+            "xmin": 0,
+            "xmax": 10
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "Num p_{T} > 30 GeV GenJets",  
+            "GetYaxis().SetTitle": "Events", 
+            "GetYaxis().SetTitleOffset": 1.3,
+            "SetMinimum" : 0.1,
+            "SetMaximum" : 40000000
+        }
+    },
+   "MET": {  
+        "Initialize": {  
+            "type": "TH1D",
+            "nbins": 120,
+            "xmin": 0,
+            "xmax": 120
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "p_{T}^{miss} [GeV]",  
+            "GetYaxis().SetTitle": "Events / GeV",
+            "GetYaxis().SetTitleOffset": 1.2  
+        }
+    },
+   "ptZ": {  
+        "Initialize": {  
+            "type": "TH1D",
+            "nbins": 120,
+            "xmin": 0,
+            "xmax": 120
+        },
+        "Attributes": {  
+            "GetXaxis().SetTitle": "p_{T}(ll) [GeV]",  
             "GetYaxis().SetTitle": "Events / GeV",
             "GetYaxis().SetTitleOffset": 1.2  
         }
