5ff6eafea861118f3692fb7232a90255337641e3
Kenneth Long <kdlong@hep.wisc.edu>
2019-07-05 23:17:02 -0400

    NanoAOD for WZxsec

---
+++ FileInfo/WZxsec2016/NanoAOD.py
@@ -0,0 +1,10 @@
+info = {
+    "wz3lnu_powheg" : {
+        "file_path" : "/data/kelong/NanoAODSkims/Unskimmed/WZ3LNu_powheg_2016/*.root",
+        "plot_group" : "wz-powheg"
+    },
+    "wz3lnu_powheg__testNano" : {
+        "file_path" : "/data/kelong/NanoAODSkims/Unskimmed/WZ3LNu_powheg_2016/*.root",
+        "plot_group" : "wz-powheg_standalone"
+    },
+}
