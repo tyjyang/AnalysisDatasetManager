c5d3e06e015a1b06d71a847e59e700a9f506cc9f
Kenneth Long <kdlong@hep.wisc.edu>
2019-07-28 14:32:12 -0500

    Add other drell yan samples

---
+++ FileInfo/montecarlo/montecarlo_drellyan.py
@@ -0,0 +1,87 @@
+info = {
+    "DYm50" : {
+        "cross_section" : 5765.4,
+        "Source of cross section" : "https://twiki.cern.ch/twiki/bin/viewauth/CMS/SummaryTable1G25ns#DY_Z",
+        "DAS Name" : "DYJetsToLL_M-50_TuneCUETP8M1_13TeV-[amcatnloFXFX OR madgraphMLM]-pythia8"
+    },
+    "DYm50_HT_70to100__cp5" : {
+        "cross_section" : 147.1,
+        "Source of cross section" : "none",
+        "DAS Name" : "DYJetsToLL_M-50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_100to200__cp5" : {
+        "cross_section" : 161.0,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_200to400__cp5" : {
+        "cross_section" : 48.58,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_400to600__cp5" : {
+        "cross_section" : 6.982,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_600to800__cp5" : {
+        "cross_section" : 1.747,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-600to800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_800to1200__cp5" : {
+        "cross_section" : 0.8052,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-800to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_1200to2500__cp5" : {
+        "cross_section" : 0.1927,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_2500toInf__cp5" : {
+        "cross_section" : 0.003478,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-2500toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_70to100" : {
+        "cross_section" : 301.0,
+        "Source of cross section" : "none",
+        "DAS Name" : "DYJetsToLL_M-50_HT-70to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_100to200" : {
+        "cross_section" : 147.4,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_200to400" : {
+        "cross_section" : 41.04,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_400to600" : {
+        "cross_section" : 5.674,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_600to800" : {
+        "cross_section" : 1.358,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-600to800_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_800to1200" : {
+        "cross_section" : 0.6229,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-800to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_1200to2500" : {
+        "cross_section" : 0.1512,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-1200to2500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+    "DYm50_HT_2500toInf" : {
+        "cross_section" : 0.003659,
+        "Source of cross section" : "XSDB",
+        "DAS Name" : "/DYJetsToLL_M-50_HT-2500toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
+    },
+}
