8eb2aeee49c94f772c8d80e3269be3c882786471
Kenneth Long <kdlong@hep.wisc.edu>
2019-09-16 14:24:13 -0400

    Add W gen DAS path

--- FileInfo/WGen/NanoAOD.py
+++ FileInfo/WGen/NanoAOD.py
@@ -1,6 +1,7 @@
 info = {
     "wlnu_mglo" : {
         "file_path" : "/store/mc/RunIIAutumn18NanoAODv4/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/70000/1780BB1F-461F-3F40-A1D9-9B5659754310.root",
-        "plot_group" : "wjets"
+        "plot_group" : "wjets",
+        "DAS" : "/WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16NanoAODv5-PUMoriond17_Nano1June2019_102X*/NANOAODSIM",
     }
 }
