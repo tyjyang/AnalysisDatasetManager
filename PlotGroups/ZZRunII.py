57d3999931d22964936653643f33ec6d9a702c05
Kenneth Long <kdlong@hep.wisc.edu>
2019-09-16 17:26:28 -0400

    Add aTGC plot group

---
+++ PlotGroups/ZZRunII.py
@@ -0,0 +1,108 @@
+info = {
+    "qqZZ_powheg" : {
+        "Name" : "q#bar{q} #rightarrow ZZ,Z#gamma^{*}",
+        "Style" : "fill-lightblue",
+        "add_perc_error" : 0.0,
+        "Members" : [
+            "zz4l-powheg"
+        ]
+    },
+    "HZZ_signal" : {
+        "Name" : "H #rightarrow ZZ",
+        "Style" : "fill-lightred",
+        "add_perc_error" : 0.0,
+        "Members" : [
+            "ggHZZ",
+            "vbfHZZ",
+            "ttH_HToZZ_4L",
+            "WminusHToZZ",
+            "WplusHToZZ",
+            "ZHToZZ_4L"
+
+        ]
+    },
+    "ggZZ" : {
+        "Name" : "gg #rightarrow ZZ,Z#gamma^{*}",
+        "Style" : "fill-blue",
+        "add_perc_error" : 0.0,
+        "Members" : [
+            "ggZZ4e",
+            "ggZZ4m",
+            "ggZZ4t",
+            "ggZZ2e2mu",
+            "ggZZ2e2tau",
+            "ggZZ2mu2tau"
+        ]
+    },
+    "dy_jets" : {
+        "Name" : "Drell-Yan",
+        "Style" : "fill-green",
+        "Members" : [
+            "DYJetsToLLM-50"
+        ]
+    },
+    "top" : {
+        "Name" : "TT",
+        "Style" : "fill-red",
+        "add_perc_error" : 0.0,
+        "Members" : [
+            "tt-jets"
+        ]
+    },
+    "wz3lnu_powheg" : {
+        "Name" : "WZ",
+        "Style" : "fill-maroon",
+        "add_perc_error" : 0.0,
+        "Members" : [
+            "wz3lnu-powheg"
+        ]
+    },
+    "VVV" : {
+        "Name" : "t#bar{t}Z,VVV",
+        "Style" : "fill-yellow",
+        "add_perc_error" : 0.0,
+        "Members" : [
+            "ZZGJetsTo4L2Nu",
+            "ZZZJetsTo4L2Nu",
+            "WZZJetsTo4L2Nu",
+            "WWZJetsTo4L2Nu"
+        ]
+    },
+    "nonprompt" : {
+        "Name" : "Z+X",
+        "Style" : "fill-green",
+        "add_perc_error" : 0.3,
+        "Members" : [
+            "DataEWKCorrected"
+        ]
+    },
+    "data" : {
+        "Name" : "Data",
+        "Style" : "data",
+        "Members" : [
+            "AllData"
+        ]
+    },
+    "data_all" : {
+        "Name" : "Data",
+        "Style" : "data",
+        "Members" : [
+            "AllData"
+        ]
+    },
+    "zzjj4l_ewk" : {
+        "Name" : "EW ZZjj",
+        "Style" : "fill-darkblue",
+        "add_perc_error" : 0.0,
+        "Members" : [
+           "ZZJJTo4L-EWK"
+        ]
+    },
+    "zz_atgc_f4_fg_0_fz_0p0015" : {
+        "Name" : "f^{z} = 0.0015",
+        "Style" : "nofill-darkpurple-dotdash",
+        "Members" : [
+            "zz4l-atgc_1"
+        ]
+    },
+}
