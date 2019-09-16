5e41c4bf3f4cdcc3b326182ac1169a5dc307b2b6
Kenneth Long <kdlong@hep.wisc.edu>
2019-09-16 17:27:12 -0400

    Add ZZ-specific MC

---
+++ FileInfo/montecarlo/montecarlo_zz.py
@@ -0,0 +1,57 @@
+info = {
+    "WWZJetsTo4L2Nu" : { 
+        "cross_section" : 0.0006024,
+        "Source of cross section" : "MCM",
+        "DAS Name" : "WWZJetsTo4L2Nu_4f_TuneCP5_13TeV_amcatnloFXFX_pythia8"
+    },
+    "WZZJetsTo4L2Nu" : { 
+        "cross_section" : 0.00027,
+        "Source of cross section" : "MCM",
+        "DAS Name" : "WZZJetsTo4L2Nu_4f_TuneCP5_13TeV_amcatnloFXFX_pythia8"
+    },
+    "ZZGJetsTo4L2Nu" : { 
+        "cross_section" : 0.001042,
+        "Source of cross section" : "MCM",
+        "DAS Name" : "ZZGJetsTo4L2Nu_4f_TuneCP5_13TeV_amcatnloFXFX_pythia8"
+    },
+    "ZZZJetsTo4L2Nu" : { 
+        "cross_section" : 0.0001907,
+        "Source of cross section" : "MCM",
+        "DAS Name" : "ZZZJetsTo4L2Nu_4f_TuneCP5_13TeV_amcatnloFXFX_pythia8"
+    },
+    "ggHZZ" : {
+        "cross_section" : 0.01218,
+        "Source of cross section" : "AN2017_342_V9",
+        "DAS Name" : "GluGluHToZZTo4L_M125_13TeV_powheg2_JHUgenV709_pythia8"
+    },
+    "vbfHZZ" : {
+        "cross_section" : 0.001044,
+        "Source of cross section" : "AN2017_342_V9",
+        "DAS Name" : "VBF_HToZZTo4L_M125_13TeV_powheg2_JHUGenV7011_pythia8"
+    },
+    "ttH_HToZZ_4L" : {
+        "cross_section" : 0.000393,
+        "Source of cross section" : "AN2017_342_V9",
+        "DAS Name" : "ttH_HToZZ_4LFilter_M125_13TeV_powheg_JHUgenV709_pythia8"
+    },
+    "WminusHToZZ" : {
+        "cross_section" : 0.000147,
+        "Source of cross section" : "AN2017_342_V9",
+        "DAS Name" : "WminusH_HToZZTo4L_M125_13TeV_powheg2-minlo-HWJ_JHUgenV709_pythia8"
+    },
+    "WplusHToZZ" : {
+        "cross_section" : 0.000232,
+        "Source of cross section" : "AN2017_342_V9",
+        "DAS Name" : "WplusH_HToZZTo4L_M125_13TeV_powheg2-minlo-HWJ_JHUgenV709_pythia8"
+    },
+    "ZHToZZ_4L" : {
+        "cross_section" : 0.000668,
+        "Source of cross section" : "AN2017_342_V9",
+        "DAS Name" : "ZH_HToZZ_4LFilter_M125_13TeV_powheg2-minlo-HZJ_JHUgenV709_pythia8"
+    },
+    "zz4l-atgc_1" : {
+        "cross_section" : 0.46,
+        "Source of cross section" : "AN2017_342_V9",
+        "kfactor" : 1.239,
+    }
+}
